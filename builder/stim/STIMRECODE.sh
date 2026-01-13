342632e56d6ec9a8eddc9a1eb00eb673d69a42e2
justatmeasthis <156380198+justatmeasthis@users.noreply.github.com>
2026-01-13 04:10:15 -0800

    Update STIMRECODE.sh

--- builder/stim/STIMRECODE.sh
+++ builder/stim/STIMRECODE.sh
@@ -29,12 +29,12 @@ else
         read -n 1 -rp "Enter: " testwarn
         if [ "$testwarn" = "Y" ] || [ "$testwarn" = "y" ]; then
             clear
-            echo "test mode is on, certain scripts wont work."
+            echo "Proceeding..."
             sleep 3
             break
         elif [ "$testwarn" = "N" ] || [ "$testwarn" = "n" ]; then
             clear
-            echo "exiting script, sorry twin"
+            echo "Exiting script, sorry twin"
             sleep 1
             exit
         else
@@ -158,11 +158,6 @@ hwid=$(crossystem hwid)
 fwid=$(crossystem fwid)
 ro_fwid=$(crossystem ro_fwid)
 fwver=$(crossystem tpm_fwver)
-gbbfldbg=$(futility gbb -g --flags "${image_file}" 2>/dev/null \
-| tail -n 1 \
-| grep -oE '0x[0-9a-fA-F]+')
-
-# -----------------------------------------
 
 # is swwp disabled??
 if flashrom --wp-disable>/dev/null 2>&1; then
@@ -183,8 +178,19 @@ else
     clear
 fi
 
-# hub tui WILL BE REPLACED ONCE NULL GETS THE GUI RIGHT
+image_file="${FLAGS_file}"
+if [ -z "${image_file}" ]; then
+    image_file="$(make_temp_file)"
+    flashrom_read "${image_file}">/dev/null 2>&1
+fi
+gbbfldbg=$(futility gbb -g --flags "${image_file}" 2>/dev/null \
+    | tail -n 1 \
+    | grep -oE '0x[0-9a-fA-F]+')
 
+# -----------------------------------------
+
+
+# hub tui WILL BE REPLACED ONCE NULL GETS THE GUI RIGHT
 while true; do
     clear
     stimascii
@@ -200,19 +206,26 @@ while true; do
     echo "3) Advanced Options" # obviously this will represent advanced options
     echo "4) Power off" # obviously this will represent power off
     read -n 1 -rp "Enter: " options # This should NEVER be a thing in the gui, only here to serve as a place holder for tui
+    
 
     case $options in
         1)
             clear
+            ic
             echo "Entering enrollment manager..."
             sleep 1
+            clear
+            clean
             enrollman
         ;;
 
         2)
             clear
+            ic
             echo "Entering daub..."
             sleep 1
+            clear
+            clean
             daub
         ;;
 
@@ -236,18 +249,22 @@ while true; do
 
                         1)
                             clear
+                            ic
                             echo "Executing Unkeyroll payload..."
                             sleep 1
                             clear
+                            clean
                             unkeyroll
                             clear
                         ;;
                         
                         2)
                             clear
+                            ic
                             echo "Executing GBBFlagger payload..."
                             sleep 1
                             clear
+                            clean
                             gbbflagger
                             clear
                         ;;
@@ -262,14 +279,17 @@ while true; do
                         
                         4)
                             clear
+                            ic
                             echo "Executing FW manager Payload..."
                             sleep 1
                             clear
+                            clean
                             fwmanager
                             clear
                         ;;
 
                         5)
+                            ic
                             clear
                             echo "$snvpd"
                             echo "$sdsdns"
@@ -280,14 +300,19 @@ while true; do
                             echo "RO_FWID = $ro_fwid"
                             echo "curtpmfwver = $fwver"
                             echo "gbb flags = $gbbfldbg"
+                            echo "SW WP = $wp"
+                            echo "HW WP = $hwwp"
                             read -n 1 -rp "Press any key to exit :D"
+                            clean
                             clear
                         ;;
 
                         6)
                             clear
+                            ic
                             echo "Heading back to main menu.."
                             sleep 1
+                            clean
                             break
                         ;;
                     esac
@@ -300,9 +325,9 @@ while true; do
                     echo
                     echo "1) WP Disable loop"
                     echo "2) Debug info"
-                    echo "3) FW manager (No full rom overwrite)"
+                    echo "3) FW manager (limited options)"
                     echo "4) Bash shell"
-                    echo "5) back"
+                    echo "5) Back"
                     read -n 1 -rp "Enter: " wponadvoptions
                     case $wponadvoptions in
 
@@ -316,15 +341,27 @@ while true; do
                         ;;
 
                         2)
+                            ic
                             clear
-                            
-                            sleep 1
+                            echo "$snvpd"
+                            echo "$sdsdns"
+                            echo "kernver = $kernver"
+                            echo "allow dev_boot_usb = $dbu"
+                            echo "HWID = $hwid"
+                            echo "FWID = $fwid"
+                            echo "RO_FWID = $ro_fwid"
+                            echo "curtpmfwver = $fwver"
+                            echo "gbb flags = $gbbfldbg"
+                            echo "SW WP = $wp"
+                            echo "HW WP = $hwwp"
+                            read -n 1 -rp "Press any key to exit :D"
+                            clean
                             clear
                         ;;
 
                         3)
                             clear
-                            echo "Executing FW manager (No full rom overwrite)"
+                            echo "Executing FW manager (limited options)"
                             sleep 1
                             fwnfrow
                             clear
