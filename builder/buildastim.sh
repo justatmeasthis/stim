fc4790f7a745f6e59fce69a1db8142cdeb70936f
justatmeasthis <156380198+justatmeasthis@users.noreply.github.com>
2026-01-12 16:56:38 -0800

    Add buildastim.sh script for building stim images

---
+++ builder/buildastim.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(dirname "$0")
+VERSION=1.0.0
+HOST_ARCH=$(uname -m)
+
+if [ $HOST_ARCH == "x86_64" ]; then
+	CGPT="$SCRIPT_DIR/bins/cgpt.x86-64"
+	SFDISK="$SCRIPT_DIR/bins/sfdisk.x86-64"
+else
+	echo "Building on an ARM system is not supported currently"
+	exit
+	# CGPT="$SCRIPT_DIR/bins/cgpt.aarch64"
+	# SFDISK="$SCRIPT_DIR/bins/sfdisk.aarch64"
+fi
+
+source $SCRIPT_DIR/functions4buildastim.sh
+
+echo "build a stim v$VERSION"
+echo "gdisk, e2fsprogs required. must be ran as root"
+[ "$EUID" -ne 0 ] && error "Please run as root"
+[ "$1" == "" ] && error "No bin specified."
+
+# Stateful is REALLY small, only about 45K with a full one.
+STATE_SIZE=$((1 * 1024 * 1024)) # 1MiB
+STATE_MNT="$(mktemp -d)"
+ROOT_MNT="$(mktemp -d)"
+LOOPDEV="$(losetup -f)"
+IMG="$1"
+
+echo "creds 2 merc workshop & kvs project, and kxtz. im highkey too lazy to code this on my own so i took it for my self, ty!!"
+
+sleep 1
+clear
+# we need this before we re-create stateful
+STATE_START=$("$CGPT" show "$IMG" | grep "STATE" | awk '{print $1}')
+suppress shrink_partitions "$IMG"
+losetup -P "$LOOPDEV" "$IMG"
+enable_rw_mount "${LOOPDEV}p3"
+
+log "Correcting GPT errors.."
+suppress fdisk "$LOOPDEV" <<EOF
+w
+EOF
+
+injcrt
+ss
+
+srnk_rt
+ss
+
+crte_steful
+ss
+
+inject_stateful
+ss
+
+uma
+ss
+
+sqsh_part "$LOOPDEV"
+ss
+
+enable_rw_mount "${LOOPDEV}p3"
+
+clean
+ss
+
+trnk_img "$IMG"
+ss
+
+log "Done stimming computer"
+trap - EXIT
